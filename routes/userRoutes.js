ce9b04b740f2bf283999036c6e91a40557cf7e37
Himanshu Sonawane <himanshusonawane1345@gmail.com>
2024-02-25 21:53:00 +0530

    get user using jwt

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.post('/login', userController.userValidation);
+router.post('/getUser', userController.getUser);
   
 
 module.exports = router;
